1f2216497baf2c3611905a15998d933f3f876844
m.dmitriev <makc-dmv@yandex.ru>
2018-12-15 14:45:59 +0500

    Изменение вопроса

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -4,15 +4,28 @@ import ActiveQuiz from "../../component/ActiveQuiz/ActiveQuiz";
 
 class Quiz extends React.Component {
   state = {
+    activeQuestion: 0,
     quiz: [
       {
         question: "Какого цвета небо?",
         rightAnswerId: 3,
+        id: 1,
         answers: [
           { text: "Черное", id: 1 },
           { text: "Синее", id: 2 },
           { text: "Голубое", id: 3 },
           { text: "Красное", id: 4 }
+        ]
+      },
+      {
+        question: "В каком году основали Санкт-Петербург?",
+        rightAnswerId: 3,
+        id: 2,
+        answers: [
+          { text: "1300", id: 1 },
+          { text: "905", id: 2 },
+          { text: "1405", id: 3 },
+          { text: "1325", id: 4 }
         ]
       }
     ]
@@ -20,6 +33,10 @@ class Quiz extends React.Component {
 
   onAnswerClickHandler = answerId => {
     console.log("answerId ", answerId);
+
+    this.setState({
+      activeQuestion: this.state.activeQuestion + 1
+    });
   };
 
   render() {
@@ -29,9 +46,11 @@ class Quiz extends React.Component {
           <h1>Quiz</h1>
 
           <ActiveQuiz
-            answers={this.state.quiz[0].answers}
-            question={this.state.quiz[0].question}
+            answers={this.state.quiz[this.state.activeQuestion].answers}
+            question={this.state.quiz[this.state.activeQuestion].question}
             onAnswerClick={this.onAnswerClickHandler}
+            quizLength={this.state.quiz.length}
+            answerNumber={this.state.activeQuestion + 1}
           />
         </div>
       </div>
