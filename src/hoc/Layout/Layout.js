fd5fca3b7e2432b1708e78ca2967dc6d0b0189f4
m.dmitriev <makc-dmv@yandex.ru>
2018-12-17 09:53:31 +0500

    Кнопка переключения меню

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,10 +1,27 @@
 import React from "react";
 import styles from "./Layout.module.css";
+import MenuToggle from '../../component/Navigation/MenuToggle/MenuToggle'
 
 class Layout extends React.Component {
+  state= {
+    menu: false
+  }
+
+  toggleMenuHandler = () => {
+    this.setState({
+      menu: !this.state.menu
+    })
+  }
+
   render() {
     return (
       <div className={styles.Layout}>
+
+        <MenuToggle
+          onToggle={this.toggleMenuHandler}
+          isOpen={this.state.menu}
+        />
+
         <main>{this.props.children}</main>
       </div>
     );
