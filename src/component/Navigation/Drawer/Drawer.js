bef75a07b83b0e560aa2915833de54f409864949
m.dmitriev <makc-dmv@yandex.ru>
2018-12-17 10:52:03 +0500

    Компонент затемнения

--- src/component/Navigation/Drawer/Drawer.js
+++ src/component/Navigation/Drawer/Drawer.js
@@ -1,8 +1,8 @@
 import React from "react";
 import styles from "./Drawer.module.css";
+import Backdrop from "../../UI/Backdrop/Backdrop";
 
 const links = [1, 2, 3];
-
 class Drawer extends React.Component {
   renderLinks() {
     return links.map((link, index) => {
@@ -20,9 +20,12 @@ class Drawer extends React.Component {
       cls.push(styles.close);
     }
     return (
-      <nav className={cls.join(' ')}>
-        <ul>{this.renderLinks()}</ul>
-      </nav>
+      <React.Fragment>
+        <nav className={cls.join(" ")}>
+          <ul>{this.renderLinks()}</ul>
+        </nav>
+        {this.props.isOpen ? <Backdrop onClick={this.props.onClose} /> : null}
+      </React.Fragment>
     );
   }
 }
