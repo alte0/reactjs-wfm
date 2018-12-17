fad48bce4329784c04caacaa2f3cddff203828d2
m.dmitriev <makc-dmv@yandex.ru>
2018-12-17 20:45:01 +0500

    Создание страниц

---
+++ src/containers/QuizCreator/QuizCreator.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class QuizCreator extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>QuizCreator</h1>
+      </div>
+    );
+  }
+}
+
+export default QuizCreator;
