b15cdd75e21d9c0b42f937f90397a4509cfa9af2
m.dmitriev <makc-dmv@yandex.ru>
2018-12-19 17:45:16 +0500

    Авторизация

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,6 +3,7 @@ import classes from "./Auth.module.css";
 import Button from "../../component/UI/Button/Button";
 import Input from "../../component/UI/Input/Input";
 import is from "is_js";
+import axios from "axios";
 
 export default class Auth extends Component {
   state = {
@@ -35,9 +36,43 @@ export default class Auth extends Component {
     }
   };
 
-  loginHandler = () => {};
+  loginHandler = async () => {
+    const authData = {
+      email: this.state.formControls.email.value,
+      password: this.state.formControls.password.value,
+      returnSecureToken: true
+    };
 
-  registerHandler = () => {};
+    try {
+      const response = await axios.post(
+        "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyBHA8koKE33APcJInH2uerNQW8a27YBXBc",
+        authData
+      );
+
+      console.log(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  registerHandler = async () => {
+    const authData = {
+      email: this.state.formControls.email.value,
+      password: this.state.formControls.password.value,
+      returnSecureToken: true
+    };
+
+    try {
+      const response = await axios.post(
+        "https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyBHA8koKE33APcJInH2uerNQW8a27YBXBc",
+        authData
+      );
+
+      console.log(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   submitHandler = event => {
     event.preventDefault();
